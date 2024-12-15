e630f971b1280cc4216686b1b1f1ffdc1f55941d
Alemi Herbert <alemiherbert@gmail.com>
2024-12-15 15:12:34 +0300

    Initial commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Minimum operations
+"""
+
+def minOperations(n: int) -> int:
+    """
+    Calculate the number of steps required to get n H characters in a file
+    """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return op
